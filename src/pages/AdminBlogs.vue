fbf670862364aa5bfe60d589f40c715c929dcb8c
Noman4455 <97723445+Noman4455@users.noreply.github.com>
2023-11-29 14:59:59 +0500

    .

---
+++ src/pages/AdminBlogs.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page>
+    <div class="text-h1">Admin Blogs</div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
---
+++ src/pages/AdminNewCar.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page>
+    <div class="text-h1">Admin New Car</div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
